11d66e6b7dd89a1dfb8a8996a8162fd8380fb3af
Goopy Flux <goopy.flux@gmail.com>
2022-08-24 12:11:30 -0500

    Add PDM install path to PATH.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 curl -sSL https://raw.githubusercontent.com/pdm-project/pdm/main/install-pdm.py | python3 -
-export PATH="$HOME/.local/bin:$PATH"
+export PATH="/opt/render/.local/bin:$PATH"
 eval "$(pdm --pep582)"
 pdm install
