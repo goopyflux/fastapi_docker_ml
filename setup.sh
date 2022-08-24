814563f29905c21caae9033632957ca0cc005be8
Goopy Flux <goopy.flux@gmail.com>
2022-08-24 14:09:07 -0500

    Update PDM to latest version.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
-curl -sSL https://raw.githubusercontent.com/pdm-project/pdm/main/install-pdm.py -v 1.15.2 | python3 -
+echo "Installing PDM"
+curl -sSL https://raw.githubusercontent.com/pdm-project/pdm/main/install-pdm.py | python3 -
 export PATH="/opt/render/.local/bin:$PATH"
 eval "$(pdm --pep582)"
+echo "Running pdm install..."
 pdm -vv install
